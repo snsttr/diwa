00d54b62e28cb2e374caaa50cb4ea128c68b0716
Tim <steufmehl@enet.eu>
2018-02-09 16:00:33 +0100

    changed footer links

--- app/config.php
+++ app/config.php
@@ -5,7 +5,7 @@
 // ============================
 $config['site']['invitation_code']      = 3702;         // this is the code that has to be used to successfully register
 $config['site']['show_welcome_message'] = false;        // show some Explanations and hints on DIWA's index Page?
-$config['site']['show_footer_info']     = false;        // show infos about author and links to github in the footer?
+$config['site']['show_footer_info']     = true;        // show infos about author and links to github in the footer?
 $config['site']['use_mail_function']    = false;        // false: saves all mails to disk only; true: also sends mails using php's mail()-function
 
 // ============================
--- app/layout/footer.php
+++ app/layout/footer.php
@@ -2,11 +2,11 @@
     <hr/>
     <div class="container">
         <footer class="text-center">
-            <div class="btn-group">
+            <div class="btn-group large">
+                <a href="?page=installation" class="btn btn-default" data-toggle="tooltip" title="Reset DIWA's Database"><i class="fa fa-bomb" aria-hidden="true"></i></a>
                 <a href="https://choosealicense.com/licenses/mit/" target="_blank" class="btn btn-default" data-toggle="tooltip" title="This Project is licensed under MIT License"><i class="fa fa-gavel" aria-hidden="true"></i></a>
-                <a href="https://github.com/snsttr/diwa" target="_blank" class="btn btn-default" data-toggle="tooltip" title="GitHub"><i class="fa fa-github" aria-hidden="true"></i></a>
+                <a href="https://github.com/snsttr/diwa" target="_blank" class="btn btn-default" data-toggle="tooltip" title="DIWA's GitHub Repository"><i class="fa fa-github" aria-hidden="true"></i></a>
                 <a href="https://github.com/snsttr/diwa/issues" target="_blank" class="btn btn-default" data-toggle="tooltip" title="Report a Bug or suggest an Improvement"><i class="fa fa-bug" aria-hidden="true"></i></a>
-                <a href="https://github.com/snsttr" target="_blank" class="btn btn-default" data-toggle="tooltip" title="DIWA's Developer"><i class="fa fa-user" aria-hidden="true"></i></a>
             </div>
         </footer>
     </div>
